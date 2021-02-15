1dc52cd9abfe93e74b578eeb395f4e677487da02
up1 <somkiat.p@gmail.com>
2021-02-15 11:55:38 +0700

    Solution

--- workshop/src/main/java/com/example/workshop/GenerateUUIDService.java
+++ workshop/src/main/java/com/example/workshop/GenerateUUIDService.java
@@ -1,7 +1,14 @@
 package com.example.workshop;
 
+import java.util.Random;
+
 public class GenerateUUIDService {
+
     public String getData() {
-        return null;
+        String result = "SOMKIAT";
+        Random random = new Random();
+        int number = random.nextInt(9);
+        return result + number;
     }
+
 }
