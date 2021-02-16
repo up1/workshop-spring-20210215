60fcd8cd8cf83cbe72496601b411da0b3f8ab518
up1 <somkiat.p@gmail.com>
2021-02-16 16:31:19 +0700

    Integrate repository with service

--- workshop2/src/main/java/com/example/workshop/HelloService.java
+++ workshop2/src/main/java/com/example/workshop/HelloService.java
@@ -1,10 +1,21 @@
 package com.example.workshop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class HelloService {
+
+    @Autowired
+    private HelloRepository helloRepository;
+
     public String getData() {
-        throw new RuntimeException("TODO");
+        Optional<Hello> hello = helloRepository.findById(1);
+        if(!hello.isPresent()) {
+            throw new RuntimeException("Data not found with id=1");
+        }
+        return hello.get().getMessage();
     }
 }
