3c73e2acdb7f640912bb306d031749e60de539e5
up1 <somkiat.p@gmail.com>
2021-02-16 17:01:10 +0700

    Testing with service and repository

--- workshop2/src/test/java/com/example/workshop/HelloController3Test.java
+++ workshop2/src/test/java/com/example/workshop/HelloController3Test.java
@@ -7,25 +7,28 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class HelloController2Test {
+public class HelloController3Test {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Autowired
+    @MockBean
     private HelloRepository helloRepository;
 
     @Test
-    public void success_with_hello_2() {
-        // Prepare data for testing
+    public void success_with_hello3() {
+        // Stubbing
         Hello data = new Hello();
         data.setId(1);
         data.setMessage("Hello World");
-        helloRepository.save(data);
+        given(helloRepository.findById(1))
+                .willReturn(Optional.of(data));
 
         // Testing
         HelloResponse result
