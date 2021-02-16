9d6b6acfe2a1e55e71da5d2ea86d7e7923fe396b
up1 <somkiat.p@gmail.com>
2021-02-16 17:58:12 +0700

    Exception handling

--- workshop2/src/main/java/com/example/workshop/HelloController.java
+++ workshop2/src/main/java/com/example/workshop/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @RestController
 public class HelloController {
 
---
+++ workshop2/src/main/java/com/example/workshop/HelloControllerHandler.java
@@ -0,0 +1,17 @@
+package com.example.workshop;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HelloControllerHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<HelloResponse> notFound( RuntimeException e ) {
+        HelloResponse response = new HelloResponse(e.getMessage());
+        return new ResponseEntity<HelloResponse>(response, HttpStatus.OK);
+    }
+
+}
