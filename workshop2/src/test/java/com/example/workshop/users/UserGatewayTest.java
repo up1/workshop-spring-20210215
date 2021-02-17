58657e2ac64fee17d769364cd6eadd6c63a5e563
up1 <somkiat.p@gmail.com>
2021-02-17 17:27:12 +0700

    Testing with gateway

---
+++ workshop2/src/test/java/com/example/workshop/users/UserGatewayTest.java
@@ -0,0 +1,29 @@
+package com.example.workshop.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserGatewayTest {
+
+    @Autowired
+    private UserGateway userGateway;
+
+    @Test
+    public void found_with_user_1() {
+        Optional<UserXResponse> user = userGateway.getUserById(1);
+        assertEquals(1, user.get().getId());
+        assertEquals("Leanne Graham", user.get().getName());
+    }
+
+    @Test
+    public void not_found_with_user_100() {
+        Optional<UserXResponse> user = userGateway.getUserById(100);
+        assertFalse(user.isPresent());
+    }
+}
