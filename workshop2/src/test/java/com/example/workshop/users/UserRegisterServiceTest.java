48d6699f8109ca8bba5d6c4708505240b4d2b526
up1 <somkiat.p@gmail.com>
2021-02-17 16:14:48 +0700

    Testing with duplicated name

--- workshop2/src/test/java/com/example/workshop/users/UserRegisterServiceTest.java
+++ workshop2/src/test/java/com/example/workshop/users/UserRegisterServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.workshop.users;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,6 +13,14 @@ public class UserRegisterServiceTest {
 
     @Autowired
     private UserRegisterService userRegisterService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @AfterEach
+    public void clearData() {
+        userRepository.deleteAll();
+    }
 
     @Test
     public void success_register() {
@@ -27,5 +37,28 @@ public class UserRegisterServiceTest {
         expected.setName("demo");
         expected.setAge(40);
         assertEquals(expected, newUser);
+    }
+
+    @Test
+    public void failure_with_duplicate_name_of_register() {
+        // Prepare data
+        User somkiat = new User();
+        somkiat.setName("demo");
+        somkiat.setAge(40);
+        userRepository.save(somkiat);
+
+        // Request for create a new user
+        UserRequest userRequest = new UserRequest();
+        userRequest.setName("demo");
+        userRequest.setAge(40);
+
+        // Call register process
+        Exception exception = assertThrows(NameDuplicateException.class, () -> {
+            userRegisterService.register(userRequest);
+        });
+
+        assertNotNull(exception);
+        assertEquals("Duplicated with username=demo", exception.getMessage());
+
     }
 }
