cf6b5fdb5d871dbca0742b601dadb65cdcce9c86
up1 <somkiat.p@gmail.com>
2021-02-17 17:20:35 +0700

    Call to user api

--- workshop2/src/main/java/com/example/workshop/users/UserGateway.java
+++ workshop2/src/main/java/com/example/workshop/users/UserGateway.java
@@ -3,7 +3,10 @@ package com.example.workshop.users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Component
 public class UserGateway {
@@ -17,4 +20,16 @@ public class UserGateway {
         this.restTemplate = restTemplate;
         this.userApiUrl = userApiUrl;
     }
+
+    public Optional<UserXResponse> getUserById(int id) {
+        String url = String.format("%s/users/%d", userApiUrl, id);
+
+        try {
+            return Optional.ofNullable(
+                    restTemplate.getForObject(url, UserXResponse.class));
+        } catch (RestClientException e) {
+            return Optional.empty();
+        }
+    }
+
 }
